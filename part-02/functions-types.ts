183ab14f27a4f328b37c3752dd027d755e24de88
Amandeep Singh <programmeramandeep@gmail.com>
2023-09-10 12:43:59 +0530

    Function types callbacks

--- part-02/functions-types.ts
+++ part-02/functions-types.ts
@@ -12,3 +12,13 @@ let combinedValues: (a: number, b: number) => number; // Function as types
 combinedValues = add;
 
 printResult(combinedValues(8, 8));
+
+// Function types callbacks
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
