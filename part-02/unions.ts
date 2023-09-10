706ae6d08ddec252a91116111f6d30faa0ea514c
Amandeep Singh <programmeramandeep@gmail.com>
2023-09-10 11:54:51 +0530

    Literals types

--- part-02/unions.ts
+++ part-02/unions.ts
@@ -1,15 +1,25 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: 'as-number' | 'as-text' // Literals
+) {
   let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
+  if (
+    (typeof input1 === 'number' && typeof input2 === 'number') ||
+    resultConversion === 'as-number'
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
 }
 
-const combineAges = combine(22, 5);
+const combineAges = combine(22, 5, 'as-number');
 console.log(combineAges);
 
-const combineNames = combine('Amandeep ', 'Singh');
+const combineStringAges = combine('22', '8', 'as-number');
+console.log(combineStringAges);
+
+const combineNames = combine('Amandeep ', 'Singh', 'as-text');
 console.log(combineNames);
