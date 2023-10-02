1f63e6791e05e77d78308c52ee7ea22f057dc576
Amandeep Singh <programmeramandeep@gmail.com>
2023-10-02 10:47:15 +0530

    Constructor function the this keyword

--- part-05/src/app.ts
+++ part-05/src/app.ts
@@ -4,7 +4,15 @@ class Department {
   constructor(name:string) {
     this.name = name;
   }
+
+  describe(this: Department) {
+    console.log(`Department: ${this.name}`);
+  }
+  
 }
 
 const accounting = new Department('Accounting');
-console.log(accounting);
+accounting.describe();
+
+const accountingCopy = {name: 'Dummy',describe: accounting.describe}
+accountingCopy.describe();
