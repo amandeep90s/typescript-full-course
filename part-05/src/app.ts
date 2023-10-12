1660a423e8384987ff90f11625c3c260366b7ffa
Amandeep Singh <programmeramandeep@gmail.com>
2023-10-12 20:55:38 +0530

    Inheritance

--- part-05/src/app.ts
+++ part-05/src/app.ts
@@ -19,8 +19,35 @@ class Department {
   }
 }
 
-const accounting = new Department('D1', 'Accounting');
-accounting.describe();
-accounting.addEmployee('Aman');
-accounting.addEmployee('Mani');
-accounting.getEmployeeInformation();
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    // Super calls the base class constructor
+    super(id, 'IT');
+  }
+}
+
+const it = new ITDepartment('D1', ['Aman']);
+console.log('Child class', it.admins);
+it.describe();
+it.addEmployee('Aman');
+it.addEmployee('Mani');
+it.getEmployeeInformation();
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+  }
+
+  addReports(report: string) {
+    this.reports.push(report);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const account = new AccountingDepartment('D2', []);
+console.log('Child class', account);
+account.addReports('Sales');
+account.printReports();
